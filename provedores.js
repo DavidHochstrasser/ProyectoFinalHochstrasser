ba5b60a6d8188ae8fbbf0630abb3e9cdd4d060c4
DavidHochstrasser <hochstrasser.davi@gmail.com>
2023-08-26 17:47:44 -0300

    correcciones de ortografia

--- provedores.js
+++ provedores.js
@@ -9,7 +9,7 @@ fetch(url)
       div.innerHTML = `
                <div class="card text-center mb-4 mt-4">
                 <div class="card-body" style="background-color: #c5dff8 !important;">
-                     <strong>Nombre usuario</strong>: ${usuario.name}
+                     <strong>Nombre</strong>: ${usuario.name}
                     <strong>Telefono</strong>: ${usuario.phone}
                       <strong>E-mail</strong>: ${usuario.email}
                       
